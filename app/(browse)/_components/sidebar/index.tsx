92bae545183616a1fe8b9eeef741fc64aaf8a75c
Anish0369 <anish231003@gmail.com>
2024-03-18 15:33:04 +0530

    Refactor browse layout and remove unused page component  Co-authored-by: rudrakshranade@gmail.com <rudrakshranade@gmail.com>

---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,14 @@
+import { Wrapper } from "./wrapper";
+import { Toggle } from "./toggle";
+
+export const Sidebar = () => {
+  return (
+    <>
+      <div className="text-white ">
+        <Wrapper>
+          <Toggle />
+        </Wrapper>
+      </div>
+    </>
+  );
+};
--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 // import Navbar from "";
 import { Navbar } from "./_components/navbar";
+import { Sidebar } from "./_components/sidebar";
+import { Container } from "./_components/container";
 
 const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="text-white">
       {/* <div>Layout</div> */}
-      <Navbar></Navbar>
+      <div>
+        <Navbar></Navbar>
+      </div>
 
-      <div className="flex h-full pt-20">{children}</div>
+      <div className="mt-[80px] flex ">
+        <Sidebar />
+        <div className="flex h-full pt-20 mt-[-80px]">
+          <Container>{children}</Container>
+        </div>
+      </div>
     </div>
   );
 };
