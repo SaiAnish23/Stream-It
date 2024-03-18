47ff492c2d4acbd02d2f075cd6cfd8e45ae04347
Anish0369 <anish231003@gmail.com>
2024-03-18 16:15:54 +0530

    Add recommended items to sidebar

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,12 +1,19 @@
 import { Wrapper } from "./wrapper";
 import { Toggle } from "./toggle";
+import { Recommended } from "./recommended";
+// import {getRecommended} from
+import { getRecommended } from "@/lib/recommended-service";
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+  const recommended = await getRecommended();
   return (
     <>
       <div className="text-white ">
         <Wrapper>
           <Toggle />
+          <div className="space-y-4 pt-4 lg:pt-0">
+            <Recommended data={recommended} />
+          </div>
         </Wrapper>
       </div>
     </>
