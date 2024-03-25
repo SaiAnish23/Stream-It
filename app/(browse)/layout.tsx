089fded89b1927048ff392457533ae79728e62e6
Anish0369 <anish231003@gmail.com>
2024-03-25 17:17:01 +0530

    Add sonner library and implement user-service and layout changes
    Co-authored-by: rudrakshranade@gmail.com <rudrakshranade@gmail.com>

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 // import Navbar from "";
 import { Navbar } from "./_components/navbar";
+// import { Sidebar } from "./_components/sidebar";
 import { Sidebar } from "./_components/sidebar";
+
 import { Container } from "./_components/container";
+
+import { Suspense } from "react";
 
 const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -14,6 +18,11 @@ const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
 
       <div className="mt-[80px] flex ">
         <Sidebar />
+
+        {/* <Suspense fallback={<SidebarSkeleton />}>
+          <Sidebar />
+        </Suspense> */}
+
         <div className="flex h-full pt-20 mt-[-80px]">
           <Container>{children}</Container>
         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 // import {dark} from "@theme-ui/presets";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Toaster position="top-right" />
+
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
